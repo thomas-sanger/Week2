50adee0b9092344a5ea58e24cc54add56b1569d0
Thomas Sanger <thomas.sanger92@gmail.com>
2025-02-13 10:51:31 -0700

    Created js function. Applied requirements to css file.

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// when the window loads, call this function to create an unordered list
+window.onload = function () {
+  // Create an unordered list
+  const ul = document.createElement("ul");
+
+  // Define list items
+  const items = [
+    "My Grandad's Fish Stew",
+    "Chili on a cold day",
+    "Broccoli Cheaddar soup from Panera",
+    "Homemade Grilled Cheese",
+    "Anything that my fiance cooks (because I don't have to ';'')'",
+  ];
+
+  // Loop thorugh each item in the array
+  items.forEach((text) => {
+    // Create a <li> element for each item
+    const li = document.createElement("li");
+    // Set the text content of the <li> element to the current item
+    li.textContent = text;
+    // Append the <li> element to the <ul>
+    ul.appendChild(li);
+  });
+
+  //Append the list to the body of the html file
+  document.body.appendChild(ul);
+};
